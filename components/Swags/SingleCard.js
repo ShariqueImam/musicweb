06ad297570acdcc85f6340eacce13c43da3e94fe
Sharique Imam <shariquegithubweb@gmail.com>
2023-08-20 23:06:04 +0500

    adsf

--- components/Swags/SingleCard.js
+++ components/Swags/SingleCard.js
@@ -20,7 +20,7 @@ const SingleCard = ({ swagData }) => {
     <FeaturedBackground1 className={style.wrapper}>
       <section className="w-full swagsItem border-t-[1px] border-t-[#F33C9A] p-2 md:px-3 rounded-tr-[20px] rounded-tl-[20px] ">
         <h2 className={style.heading}>{swagData.name}</h2>
-        <h2 className={style.para}>{swagData.price}</h2>
+        <h2 className={style.para}>$ {swagData.price}</h2>
         <ReactStars count={5} size={12} color1={"#ffff00"} edit={false} />
       </section>
     </FeaturedBackground1>
