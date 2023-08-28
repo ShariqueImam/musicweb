5cead6322c94c45cc1494b46fe6df7eb356bda39
Sharique Imam <shariquegithubweb@gmail.com>
2023-08-28 23:04:27 +0500

    af

--- components/Live/Station.js
+++ components/Live/Station.js
@@ -2,22 +2,22 @@ import React from "react";
 import Link from "next/link";
 const Station = () => {
   return (
-    <div className="z-0">
+    <div className="z-30">
       <div className="z-0">
         <img
           src="/assets/circle.png"
           alt=""
-          className="absolute z-0 mt-[-28rem]  w-[40rem] ml-[30rem]"
+          className="absolute z-0 mt-[-28rem]  w-[40rem] ml-[30rem] hidden md:flex"
         />
         <img
           src="/assets/circle2.png"
           alt=""
-          className="absolute z-0 mt-[-20rem] w-[40rem] ml-[-50rem]"
+          className="absolute z-0 mt-[-20rem] w-[40rem] ml-[-50rem] hidden md:flex"
         />
         <img
           src="/assets/blue.png"
           alt=""
-          className="absolute z-0 md:ml-[70rem] mt-[20rem] md:mt-[-40rem]"
+          className="absolute z-0 md:ml-[70rem] mt-[20rem] md:mt-[-40rem] hidden md:flex"
         />{" "}
       </div>
       <div className="flex flex-col items-center justify-center z-30">
@@ -28,7 +28,7 @@ const Station = () => {
           className="mx-auto w-[20rem] md:w-[25rem] my-20"
         />
         <Link href="https://youtu.be/UqmUxkRPBS0?si=sXtpbAMUPj-foVYe">
-          <button className="exploreBtn text-white px-12 md:px-16 py-3 md:py-4 my-8 flex items-center justify-center z-50">
+          <button className="exploreBtn text-white px-12 md:px-16 py-3 md:py-4 my-8 flex items-center justify-center z-50 text-2xl">
             <img src="/assets/light.png" alt="" className="w-8 mr-4" />
             Live
             <img src="/assets/light.png" alt="" className="w-8 ml-4" />
